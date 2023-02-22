90eb9f2b022f7186da6617fed7cf9804e30885bd
DynamoUltima <dynmao.joey@gmail.com>
2023-02-22 08:03:37 +0000

    landing page

--- comps/navbar/navigationbar.tsx
+++ comps/navbar/navigationbar.tsx
@@ -10,15 +10,15 @@ const Navigationbar = () => {
 
                 <div className='w-20 h-14 relative'><Image alt="logo" layout="contain" src={gradcelaImage} /></div>
                 <div className="flex flex-col lg:flex-row justify-center  lg:justify-between items-center gap-5">
-                    <p>Courses</p>
+                    <p>Services</p>
                     <p>About Us</p>
                     <p>Teacher</p>
                     <p>Pricing</p>
-                    <p>Careers</p>
-                    <p>Blog</p>
+                    {/* <p>Careers</p>
+                    <p>Blog</p> */}
                 </div>
                 <button className='btn bg-white text-teal-300 border-none hover:bg-[#2DD4BF] p-2 hover:text-white capitalize rounded-full'>
-                    Get Started
+                   Coming Soon
                 </button>
             </div>
 
