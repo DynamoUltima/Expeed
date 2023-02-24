90254e32f6f8c39b46d0f2bb10a1c528486a8709
DynamoUltima <dynmao.joey@gmail.com>
2023-02-24 17:46:52 +0000

    text and image updated

--- comps/navbar/navigationbar.tsx
+++ comps/navbar/navigationbar.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import gradcelaImage from "/assets/svg/gradcela logo.svg"
+import gradcelaImage from "/assets/svg/GradcelaLogo.svg"
 
 const Navigationbar = () => {
     return (
